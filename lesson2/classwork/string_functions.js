1b2ff1a9fc9f5914ff124845360462d56495a81c
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-06-15 00:21:01 +0000

    string_functions

---
+++ lesson2/classwork/string_functions.js
@@ -0,0 +1,18 @@
+let message = "Hello JavaScript";
+console.log(message);
+
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+
+console.log(message.includes("Java"));
+console.log(message.includes("Python"));
+
+console.log(message.indexOf("J"));
+console.log(message.indexOf("z"))
+
+console.log(message.slice(0, 5));
+console.log(message.slice(6));
+
+let spaced = "  hello  ";
+console.log(spaced.trim());
+
