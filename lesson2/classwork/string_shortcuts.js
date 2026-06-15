12f6d3de3f67ed41b002cb8605094b4c8ab83429
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-06-15 00:27:34 +0000

    string shortcuts

---
+++ lesson2/classwork/string_shortcuts.js
@@ -0,0 +1,17 @@
+let word = "strawberry";
+
+console.log(word.slice(0, 5));
+console.log(word.slice(5));
+console.log(word.slice(-5));
+
+let first = "Java";
+let second = "Script";
+let language = first + second;
+console.log(language);
+
+let name = "Alex";
+let greeting = "Hello, " + name + "!";
+console.log(greeting);
+
+let age = 13;
+console.log(`${name} is ${age} years old`);
