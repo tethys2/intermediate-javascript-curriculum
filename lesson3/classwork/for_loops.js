e19f74051ddbb32b58de7ed9068ee5c2ae7da03c
Toby Hansen <167672561+tethys2@users.noreply.github.com>
2026-06-22 00:11:29 +0000

    for_loops

---
+++ lesson3/classwork/for_loops.js
@@ -0,0 +1,16 @@
+for (let i = 3; i < 8; i++){
+    console.log(i);
+}
+console.log("Loop 2");
+for (let i = 0; i <= 10; i += 2){
+    console.log(i);
+}
+console.log("Loop 3");
+for (let i = 5; i >= 1; i--){
+    console.log(i);
+}
+console.log("Loop 4");
+let word = "banana";
+for (let i = word.length - 1; i >= 0; i--){
+    console.log(word[i]);
+}
